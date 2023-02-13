4bb70464d37758b67d7cb17f3763798cc1aef901
Gwada33 <gwadagwada22@gmail.com>
2023-02-13 19:10:50 -0400

    First commit
    Commit all

---
+++ app.js
@@ -0,0 +1,37 @@
+const titreSpans = document.querySelectorAll('h1 span');
+const btns = document.querySelectorAll('.btn-first');
+const logo = document.querySelector('.logo');
+const medias = document.querySelectorAll('.bulle');
+const l1 = document.querySelector('.l1');
+const l2 = document.querySelector('.l2');
+const text = document.querySelectorAll('.text');
+const logo2 = document.querySelector(".logo2");
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused: true});
+
+    TL
+    .staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+    .staggerFrom(btns, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1')
+    .from(l1, 1, {width: 0, ease: "power2.out"}, '-=2')
+    .from(l2, 1, {width: 0, ease: "power2.out"}, '-=2')
+    .from(logo, 0.4, {transform: "scale(0)", ease: "power2.out"}, '-=2')
+    .staggerFrom(medias, 1, {transform: "scale(0.1)", opacity: 0, ease: "power2.out"}, 0.3, '-=1')
+    .staggerFrom(text, 1,  {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+    .from(logo2, 0.8, {transform: "scale(0)", ease: "power2.out"}, '-=2');
+    
+
+    TL.play();
+})
---
+++ stretch_sans/readme.txt
@@ -0,0 +1,18 @@
+NOTE: Stretch Sans Font is for PERSONAL USE ONLY!
+Please contact me before any commercial use.
+My fonts for free use are allowed only in personal projects, and non-profit.
+If you make money from using my fonts, Please purchase a commercial license.
+Contact me at hi@riljs.com
+If you want DONATE click here http://www.paypal.me/KhairilAnwar
+I really appreciate your donations.
+Thank you 
+
+Link to purchase full version and commercial license :
+https://www.creativefabrica.com/designer/riljs/ref/365620
+
+Please visit our store for more great fonts:
+https://www.creativefabrica.com/ref/365620
+
+https://www.behance.net/riljs
+
+
